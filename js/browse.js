0b86d295d0b6c51e330273e58deed39028c1227b
duerain3630 <147635522+duerain3630@users.noreply.github.com>
2026-06-27 07:55:41 -0400

    Update browse.js

--- js/browse.js
+++ js/browse.js
@@ -1,1 +1,55 @@
+const params = new URLSearchParams(window.location.search);
 
+const type = params.get("type");
+
+fetch("./database.json")
+.then(response => response.json())
+.then(database => {
+
+    const title = document.getElementById("pageTitle");
+    const count = document.getElementById("itemCount");
+    const list = document.getElementById("list");
+
+    if (!database[type]) {
+
+        title.textContent = "Not Found";
+        return;
+
+    }
+
+    title.textContent =
+        type.charAt(0).toUpperCase() +
+        type.slice(1);
+
+    count.textContent =
+        database[type].length + " items";
+
+    database[type].forEach(item => {
+
+        const div = document.createElement("div");
+
+        div.className = "browse-item";
+
+        if (type === "games") {
+
+            div.innerHTML =
+                `<a href="game.html?id=${item.id}">
+                    ${item.title}
+                </a>`;
+
+        }
+
+        else {
+
+            div.innerHTML =
+                `<a href="entity.html?type=${type.slice(0,-1)}&id=${item.id}">
+                    ${item.name || item.year}
+                </a>`;
+
+        }
+
+        list.appendChild(div);
+
+    });
+
+});
